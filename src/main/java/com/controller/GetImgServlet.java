648d4f90e7a2e5ca2e6ed73cbdb650ae3069f632
hkjjsgdd <2370837256@qq.com>
2021-06-01 14:04:33 +0800

    Home13 Work-Code Exercise#2 Commit

--- src/main/java/com/controller/GetImgServlet.java
+++ src/main/java/com/controller/GetImgServlet.java
@@ -11,7 +11,7 @@ import java.io.IOException;
 import java.io.OutputStream;
 import java.sql.Connection;
 import java.sql.SQLException;
-    
+
 @WebServlet(name = "GetImgServlet",value = "/getImg")
 public class GetImgServlet extends HttpServlet {
     Connection con = null;
